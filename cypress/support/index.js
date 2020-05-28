c4d2b56eb9152246b6b0bca10d9b800eae199d9a
Andrew Andrewsson <sidor@adamed.com>
2020-05-28 14:32:11 +0200

    add tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,3 +5,7 @@ import './commands/commandsLogin'
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   })
+
+  Cypress.Screenshot.defaults({
+    screenshotOnRunFailure: false
+  })
